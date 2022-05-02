4d673d5010d0695ab01873762adf7fae17f13f76
miguel.angel.jimenez <miguelj@infoexpo.com.mx>
2022-05-02 16:49:36 -0500

    Example crud

--- README.md
+++
@@ -1,4 +0,0 @@
-# API-EXPRESS
-## step 1
-### install packages
-<code>npm install</code>
---
+++ config/db.config.js
@@ -0,0 +1,13 @@
+'use strict';
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'api_test'
+});
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Database Connected!");
+});
+module.exports = connection;
